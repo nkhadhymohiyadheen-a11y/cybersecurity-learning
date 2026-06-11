a0334f6dea624f9499d205bfd6737347c717dd93
nkhadhymohiyadheen-a11y <nkhadhymohiyadheen@gmail.com>
2026-06-12 00:35:07 +0530

    Add basic port scanner functionality

---
+++ python-projects/port-scanner/port_scanner.py
@@ -0,0 +1,17 @@
+import socket
+
+target = input("Enter IP address: ")
+
+print(f"\nScanning {target}...\n")
+
+for port in range(1, 101):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    result = s.connect_ex((target, port))
+
+    if result == 0:
+        print(f"Port {port} is OPEN")
+
+    s.close()
+
+print("\nScan Complete!")
