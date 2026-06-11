2ac35a305dc189b61002cb40e3250716bb320ade
nkhadhymohiyadheen-a11y <nkhadhymohiyadheen@gmail.com>
2026-06-12 00:40:41 +0530

    Update port_scanner.py

--- python-projects/port-scanner/port_scanner.py
+++ python-projects/port-scanner/port_scanner.py
@@ -7,6 +7,8 @@ print(f"\nScanning {target}...\n")
 for port in range(1, 101):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+    s.settimeout(0.1)
+
     result = s.connect_ex((target, port))
 
     if result == 0:
